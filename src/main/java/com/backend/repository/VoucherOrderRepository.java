8e2ebd44062858a17d135cef6012ad4d3492ade8
Trương Mạnh Quang <truongmanhquang2k3@gmail.com>
2023-10-03 11:09:12 +0700

    getAllVoucher

--- src/main/java/com/backend/repository/VoucherOrderRepository.java
+++ src/main/java/com/backend/repository/VoucherOrderRepository.java
@@ -1,9 +1,18 @@
 package com.backend.repository;
 
+import com.backend.dto.response.VoucherOrderResponse;
 import com.backend.entity.VoucherOrder;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.Tuple;
+import java.util.List;
+
 @Repository
-public interface VoucherOrderRepository extends JpaRepository<VoucherOrder,Long> {
+public interface VoucherOrderRepository extends JpaRepository<VoucherOrder, Long> {
+    @Query(value = "SELECT v.code,v.name, v.quantity, v.discountamount, v.minbillvalue, v.startdate, v.enddate ,v.createdate ,v.updateat ,v.reduceform ,v.status FROM voucherorder v ",nativeQuery = true)
+    Page<Tuple> getAllVoucherOrder(Pageable pageable);
 }
